7a31b1e013618507cd1946eda43ab6d623c31a5e
Marshall Quander <marshall@quixey.com>
2014-06-22 14:21:43 -0700

    Add basic project functionality to exercise

---
+++ src/projects.js
@@ -0,0 +1,61 @@
+function Project(id, type, name, lastActivity) {
+    this.id = id;
+    this.type = type;
+    this.name = name;
+    this.lastActivity = lastActivity;
+}
+
+$(function() {
+    // The list of all projects currently in the system.
+    // (Feel free to imagine this came from a database somewhere on page load.)
+    var CURRENT_PROJECTS = [
+        new Project(0, "Training", "Patrick's experimental branch", new Date(2014, 6, 17, 13, 5, 842)),
+        new Project(1, "Testing", "Blind test of autosuggest model", new Date(2014, 6, 21, 18, 44, 229))
+    ];
+
+    // The current maximum ID, so we know how to allocate an ID for a new project.
+    // (Yes, the database should be taking care of this, too.)
+    var MAX_ID = Math.max.apply(null, $.map(CURRENT_PROJECTS, function(pj) { return pj.id; }));
+
+    // Loads a list of projects into the UI.
+    var loadProjects = function($container, projects) {
+        $.fn.append.apply($container, $.map(projects, function(pj) {
+            return $("<tr>").append(
+                $("<td>").text(pj.id),
+                $("<td>").text(pj.type),
+                $("<td>").text(pj.name),
+                $("<td>").text(pj.lastActivity.toString())
+            );
+        }));
+    };
+
+    // Creates a new project based on the user input in the form.
+    var createProject = function($form) {
+        return new Project(
+            MAX_ID + 1,
+            $form.find("#project-type").val(),
+            $form.find("#project-name").val(),
+            new Date()
+        );
+    };
+
+    // Clears the data in the form so that it's easy to enter a new project.
+    var resetForm = function($form) {
+        $form.find("#project-type").val("");
+        $form.find("#project-name").val("");
+        $form.find("input:first").focus();
+    };
+
+    var $projectTable = $("#project-list>tbody");
+    loadProjects($projectTable, CURRENT_PROJECTS);
+
+    $("#add-project-form").submit(function(e) {
+        var $form = $(this);
+        pj = createProject($form);
+        MAX_ID = pj.id;
+        CURRENT_PROJECTS.push(pj);
+        loadProjects($projectTable, [pj]);
+        resetForm($form);
+        e.preventDefault();
+    });
+});
