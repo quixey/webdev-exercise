454be81ba26af84a454877940ca9b634e3c8bf7b
Rayliu521 <y_liu13@u.pacific.edu>
2014-09-03 17:06:06 -0700

    Check if the project is up-to-date using github api

--- src/projects.js
+++ src/projects.js
@@ -1,15 +1,16 @@
-function Project(id, type, name, lastActivity) {
+function Project(id, type, name, lastActivity, status) {
     this.id = id;
     this.type = type;
     this.name = name;
     this.lastActivity = lastActivity;
+    this.status = status;
 }
 
 // The list of all projects currently in the system.
 // (Feel free to imagine this came from a database somewhere on page load.)
 var CURRENT_PROJECTS = [
-    new Project(0, "Training", "Patrick's experimental branch", new Date(2014, 6, 17, 13, 5, 842)),
-    new Project(1, "Testing", "Blind test of autosuggest model", new Date(2014, 6, 21, 18, 44, 229))
+    new Project(0, "Training", "Patrick's experimental branch", new Date(2014, 6, 17, 13, 5, 842), "testStatus"),
+    new Project(1, "Testing", "Blind test of autosuggest model", new Date(2014, 6, 21, 18, 44, 229), "testStatus")
 ];
 
 // The current maximum ID, so we know how to allocate an ID for a new project.
@@ -23,18 +24,35 @@ $(function(){
                 $("<td>").text(pj.id),
                 $("<td>").text(pj.type),
                 $("<td>").text(pj.name),
-                $("<td>").text(pj.lastActivity.toString())
+                $("<td>").text(pj.lastActivity.toString()),
+                $("<td>").text(pj.status)
             );
         }));
     };
 
+    var getInfo = function(project_branch_name) {
+        url = "https://api.github.com/repos/quixey/webdev-exercise/compare/baseline-branch..." + project_branch_name;
+        var request = null;
+        request = new XMLHttpRequest();
+        request.open("GET", url, false);
+        request.send(null);
+        res = JSON.parse(request.responseText);
+        if(res.message == "Not Found") {
+            alert("Invalid Branch Name");
+            return "";
+        }
+            
+        
+        return res.status;
+    }
     // Creates a new project based on the user input in the form.
     var createProject = function($form) {
         return new Project(
             MAX_ID + 1,
             $form.find("#project-type").val(),
             $form.find("#project-name").val(),
-            new Date()
+            new Date(),
+            getInfo($form.find("#project-name").val())
         );
     };
 
@@ -52,9 +70,12 @@ $(function(){
         var $form = $(this);
         pj = createProject($form);
         MAX_ID = pj.id;
-        CURRENT_PROJECTS.push(pj);
-        loadProjects($projectTable, [pj]);
-        resetForm($form);
-        e.preventDefault();
+        if(pj.status != "") {
+            CURRENT_PROJECTS.push(pj);
+            loadProjects($projectTable, [pj]);
+            resetForm($form);
+            e.preventDefault();
+        } 
+        
     });
 });
