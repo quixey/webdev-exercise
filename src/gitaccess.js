1549fc67575ab728016463e7d37e192ea108b796
[01;31m[K"name"[m[K: "Andrew Donato <[01;31m[K"email"[m[K: "andrew.donato91@gmail.com>
2015-10-20 12:51:09 -0700

    made the http request  work

--- src/gitaccess.js
+++ src/gitaccess.js
@@ -1,3 +1,8 @@
+var request = new XMLHttpRequest();
+request.open('get', 'https://api.github.com/users/andrewdonato')
+request.send()
+
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
