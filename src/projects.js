e8a18f13f0a598e5e0d5a3b922be7880ffa2dc3a
1UnboundedSentience <jsnx21@gmail.com>
2015-10-21 17:01:50 -0700

    displaying git commits

--- src/projects.js
+++ src/projects.js
@@ -1,8 +1,9 @@
-function Project(id, type, name, lastActivity) {
+function Project(id, type, name, lastActivity, gitDiff) {
     this.id = id;
     this.type = type;
     this.name = name;
     this.lastActivity = lastActivity;
+    this.gitDiff = gitDiff;
 }
 
 // The list of all projects currently in the system.
@@ -23,25 +24,47 @@ $(function(){
                 $("<td>").text(pj.id),
                 $("<td>").text(pj.type),
                 $("<td>").text(pj.name),
-                $("<td>").text(pj.lastActivity.toString())
+                $("<td>").text(pj.lastActivity.toString()),
+                $("<td>").text(pj.gitDiff)
             );
         }));
     };
 
+    var githubCall = function(githubCompare) {
+
+        var differences = 0;
+        $.ajax({
+                method: 'GET',
+                url: githubCompare
+        }).done(function(response) {
+            differences = response['ahead_by'];
+            console.log(differences);
+            return differences;
+        }).fail(function(response) {
+            return "Request to GitHub failed"
+        });
+    }
     // Creates a new project based on the user input in the form.
     var createProject = function($form) {
+        var githubUsername = $form.find("#github-username").val();
+        var githubProject = $form.find("#github-project").val();
+        var githubBranch = $form.find("#github-new-branch").val();
+        var githubCompare = "https://api.github.com/repos/quixey/webdev-exercise/compare/master..." + githubBranch
         return new Project(
             MAX_ID + 1,
             $form.find("#project-type").val(),
             $form.find("#project-name").val(),
-            new Date()
+            new Date(),
+            githubCall(githubCompare)
         );
+
     };
 
     // Clears the data in the form so that it's easy to enter a new project.
     var resetForm = function($form) {
         $form.find("#project-type").val("");
         $form.find("#project-name").val("");
+        $form.find("#github-new-branch").val("");
         $form.find("input:first").focus();
     };
 
